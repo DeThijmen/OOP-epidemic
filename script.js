726bb91ef51d10ab8bd8c22df6a661b92cb293e8
DeThijmen <112439@emmauscollege.nl>
2023-09-01 07:16:11 +0000

    veel

--- script.js
+++ script.js
@@ -12,12 +12,11 @@
 /* ********************************************* */
 /* globale variabelen die je gebruikt in je game */
 /* ********************************************* */
-var x;
-var y;
-var speedX;
-var speedY;
+var speedsX;
+var speedsY;
+var xPosities;
+var yPosities;
 const BREEDTE = 20;
-
 
 
 /* ********************************************* */
@@ -34,10 +33,10 @@ function setup() {
   createCanvas(1280, 720);
 
   // initialiseer waarden
-  x = width / 2;               // midden van de breedte van het canvas
-  y = height / 2;              // midden van de hoogte van het canvas
-  speedX = random(-5, 5);      // random waarde tussen -5 en 5
-  speedY = random(-5, 5);      // 👆
+  xPosities = [width / 2, random(200, 300), random(300, 400), random(400, 500), random(500, 600), random(600, 700), random(700, 800), random(800, 900), random(900, 1000)];
+  yPosities = [height / 2, random(150, 200), random(200, 250), random(250, 300), random(300, 350), random(400, 450), random(450, 500), random(500, 550), random(550, 600)];
+  speedsX = [random(-10, 10), random(-10, 10), random(-10, 10), random(-10, 10), random(-10, 10), random(-10, 10), random(-10, 10), random(-10, 10), random(-10, 10)];      // random waardes tussen -10 en 10
+  speedsY = [random(-10, 10), random(-10, 10), random(-10, 10), random(-10, 10), random(-10, 10), random(-10, 10), random(-10, 10), random(-10, 10), random(-10, 10)];      // 👆
 }
 
 /**
@@ -45,26 +44,33 @@ function setup() {
  * de code in deze functie wordt 50 keer per seconde
  * uitgevoerd door de p5 library, nadat de setup functie klaar is
  */
-function draw() {
-  // zwarte achtergrond
-  background(0, 0, 0);
 
-  // teken
-  noStroke;
-  fill(255, 255, 255);
-  rect(x, y, BREEDTE, BREEDTE);
+  function draw() {
+    // zwarte achtergrond
+    background(0, 0, 0);
 
-  // update positie
-  x = x + speedX;
-  y = y + speedY;
+    for (var i = 0; i < xPosities.length; i++) {
+    // teken
+    noStroke;
+    fill(255, 255, 255);
+    rect(xPosities[i], yPosities[i], BREEDTE, BREEDTE);
 
-  // stuiter evt. tegen de kanten
-  if (x <= 0 || x + BREEDTE >= width) {
-    speedX = speedX * -1;
-  }
+    fill(255, 100, 100);
+    rect(xPosities[0], yPosities[0], BREEDTE, BREEDTE);
 
-  if (y <= 0 || y + BREEDTE >= height) {
-    speedY = speedY * -1;
+    // update positie
+    xPosities[i] = xPosities[i] + speedsX[i];
+    yPosities[i] = yPosities[i] + speedsY[i];
+
+    // stuiter evt. tegen de kanten
+    if (xPosities[i] <= 0 || xPosities[i] + BREEDTE >= width) {
+    speedsX[i] = speedsX[i] * -1;
+    }
+
+    if (yPosities[i] <= 0 || yPosities[i] + BREEDTE >= height) {
+      speedsY[i] = speedsY[i] * -1;
+    }
+
   }
 
 }
