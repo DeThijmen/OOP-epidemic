0a462efcd41fed29af9702cfc92f26f0efcabe1a
DeThijmen <112439@emmauscollege.nl>
2023-09-15 07:15:36 +0000

    iets

--- script.js
+++ script.js
@@ -12,11 +12,10 @@
 /* ********************************************* */
 /* globale variabelen die je gebruikt in je game */
 /* ********************************************* */
-var speedsX;
-var speedsY;
-var xPosities;
-var yPosities;
 const BREEDTE = 20;
+var mensen;
+
+
 
 
 /* ********************************************* */
@@ -32,11 +31,17 @@ function setup() {
   // Maak een canvas (rechthoek) waarin je je speelveld kunt tekenen
   createCanvas(1280, 720);
 
-  // initialiseer waarden
-  xPosities = [width / 2, random(200, 300), random(300, 400), random(400, 500), random(500, 600), random(600, 700), random(700, 800), random(800, 900), random(900, 1000)];
-  yPosities = [height / 2, random(150, 200), random(200, 250), random(250, 300), random(300, 350), random(400, 450), random(450, 500), random(500, 550), random(550, 600)];
-  speedsX = [random(-10, 10), random(-10, 10), random(-10, 10), random(-10, 10), random(-10, 10), random(-10, 10), random(-10, 10), random(-10, 10), random(-10, 10)];      // random waardes tussen -10 en 10
-  speedsY = [random(-10, 10), random(-10, 10), random(-10, 10), random(-10, 10), random(-10, 10), random(-10, 10), random(-10, 10), random(-10, 10), random(-10, 10)];      // 👆
+  var mensen = [{x : width / 2, y : height / 2, speedX : random(-10, 10), speedY : random(-10, 10)},
+            {x : random(200, 300), y : random(100, 150), speedX : random(-10, 10), speedY : random(-10, 10)},
+            {x : random(300, 400), y : random(150, 250), speedX : random(-10, 10), speedY : random(-10, 10)},
+            {x : random(400, 500), y : random(250, 300), speedX : random(-10, 10), speedY : random(-10, 10)},
+            {x : random(500, 600), y : random(300, 400), speedX : random(-10, 10), speedY : random(-10, 10)},
+            {x : random(600, 700), y : random(400, 450), speedX : random(-10, 10), speedY : random(-10, 10)},
+            {x : random(700, 800), y : random(450, 500), speedX : random(-10, 10), speedY : random(-10, 10)},
+            {x : random(800, 900), y : random(500, 600), speedX : random(-10, 10), speedY : random(-10, 10)},
+            {x : random(900, 1000), y : random(600, 700), speedX : random(-10, 10), speedY : random(-10, 10)}
+          ];
+
 }
 
 /**
@@ -49,26 +54,26 @@ function setup() {
     // zwarte achtergrond
     background(0, 0, 0);
 
-    for (var i = 0; i < xPosities.length; i++) {
+    for (var i = 0; i < x.length; i++) {
     // teken
     noStroke;
     fill(255, 255, 255);
-    rect(xPosities[i], yPosities[i], BREEDTE, BREEDTE);
+    rect(x[i], y[i], BREEDTE, BREEDTE);
 
     fill(255, 100, 100);
-    rect(xPosities[0], yPosities[0], BREEDTE, BREEDTE);
+    rect(x[0], y[0], BREEDTE, BREEDTE);
 
     // update positie
-    xPosities[i] = xPosities[i] + speedsX[i];
-    yPosities[i] = yPosities[i] + speedsY[i];
+    mensen[i].x = mensen[i].x + mensen[i].speedX;
+    mensen[i].y = mensen[i].y + mensen[i].speedY;
 
     // stuiter evt. tegen de kanten
-    if (xPosities[i] <= 0 || xPosities[i] + BREEDTE >= width) {
-    speedsX[i] = speedsX[i] * -1;
+    if (mensen[i].x <= 0 || mensen[i].x + BREEDTE >= width) {
+    mensen[i].speedX = mensen[i].speedX * -1;
     }
 
-    if (yPosities[i] <= 0 || yPosities[i] + BREEDTE >= height) {
-      speedsY[i] = speedsY[i] * -1;
+    if (mensen[i].y <= 0 || mensen[i].y + BREEDTE >= height) {
+      mensen[i].speedY = mensen[i].speedY * -1;
     }
 
   }
